255b995d76b692662ecad6bde5c4c82b091f8200
mateuszebendo <mateuszebendovp30@gmail.com>
2024-04-02 14:47:59 -0300

    push more exercices

---
+++ Programacao_Orientada_Objeto/screenmatch/src/main/Principal.java
@@ -0,0 +1,30 @@
+package main;
+ 
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+
+
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("O poderoso chefao");
+        meuFilme.setAnoDeLancamento(1978);
+        meuFilme.setDuracaoEmMinutos(180);
+
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+            System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
+            System.out.println(meuFilme.pegaMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
+        }
+
+    }
